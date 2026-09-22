5120b6e03e6733707ab570cbbfc1c785a913ec3e
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-22 13:51:28 -0400

    Finished Calculator + CalculatorTest

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,17 @@
 public class Calculator {
-    
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static double add(double a, double b){
+        return a+b;
+    }
+
+    public static int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static String add(String a, String b){
+        return a+b;
+    }
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,3 +1,13 @@
 public class CalculatorTest {
-    
+    public static void main(String args[]){
+        Calculator newCalculator = new Calculator();
+
+        System.out.print(newCalculator.add(1, 2)+"\n"); //Adds two integers
+
+        System.out.print(newCalculator.add(1.53, 7.56)+"\n"); //Adds two doubles
+
+        System.out.print(newCalculator.add(1, 2, 3)+"\n"); //Adds three integers
+
+        System.out.print(newCalculator.add("Hello ", "World"));
+    }
 }
